20c9d4fb5158d8febc614cca197d80a8417ce261
Ezapien07 <zapienernesto5@gmail.com>
2021-04-16 17:25:16 -0500

    verificar el login

--- Forms.py
+++ Forms.py
@@ -35,3 +35,34 @@ class ClienteForm(Form):
     ])
     anio=IntegerField('Año', [
         validators.required(message='El campo es requerido ')])
+
+class ProveedorForm(Form):
+    id=IntegerField('id')
+    empresa = StringField('Empresa',[
+        validators.required(message='El nombre de la empresa es requerido'),
+        validators.length(min=3,max=25, message="El nombre no cumple con los solicitado")
+    ])
+    contacto=StringField('Contacto',[
+        validators.required(message='El nombre del contacto es requerido'),
+        validators.length(min=3,max=25,message="El nombre del contacto no valido")
+    ])
+    calle=StringField('Calle',[
+        validators.required(message='La calle es requerida'),
+        validators.length(min=2,max=25,message="La calle no es valida")
+    ])
+    colonia=StringField('Colonia',[
+        validators.required(message='La colonia es requerida'),
+        validators.length(min=2,max=25,message="La colonia no es valida")
+    ])
+    Municipio=StringField('Municipio',[
+        validators.required(message="El municipio es requerido"),
+        validators.length(min=2,max=25,message="El municipio no es valido")
+    ])
+    Estado=StringField('Estado',[
+        validators.required(message="El estado es requerido"),
+        validators.length(min=2,max=25,message="El estado no es valido")
+    ])
+    Telefono=StringField('Telefono',[
+        validators.required(message="El telefono es requerido"),
+        validators.length(min=9,max=13,message="El telefono no es valido")
+    ])
